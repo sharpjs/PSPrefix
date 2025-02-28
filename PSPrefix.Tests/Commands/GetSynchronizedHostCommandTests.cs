665318e3a8c867966697ebff5c7f8302c2778c1e
Jeff Sharp <public@sharpjs.net>
2025-02-28 07:56:30 -0800

    Fix coverage gap.

--- PSPrefix.Tests/Commands/GetSynchronizedHostCommandTests.cs
+++ PSPrefix.Tests/Commands/GetSynchronizedHostCommandTests.cs
@@ -3,6 +3,7 @@
 
 namespace PSPrefix.Commands;
 
+using static ConsoleColor;
 using static ScriptExecutor;
 
 [TestFixture]
@@ -21,5 +22,42 @@ public class GetSynchronizedHostCommandTests : CommandTests
             .BaseObject.ShouldBeOfType<SynchronizedHost>()
             .UI        .ShouldNotBeNull()
             .WriteLine();
+    }
+
+    [Test]
+    public void UseInIntegrationTest()
+    {
+        const string ElapsedRegex = @"^\[\+[0-9]{2}:[0-9]{2}:[0-9]{2}\] $";
+
+        RawUI.SetupProperty(u => u.ForegroundColor);
+        RawUI.SetupProperty(u => u.BackgroundColor);
+
+        RawUI.Object.ForegroundColor = White;
+        RawUI.Object.BackgroundColor = Black;
+
+        UI.Setup(u => u.Write    (DarkGray, Black, It.IsRegex(ElapsedRegex))).Verifiable();
+        UI.Setup(u => u.Write    (DarkBlue, Black, "[Inner] "              )).Verifiable();
+        UI.Setup(u => u.WriteLine(White,    Black, "Foo"                   )).Verifiable();
+
+        var (output, exception) = Execute(
+            Host.Object,
+            // NOTE: Be careful to use the PSPrefix build under test, not any
+            // version that happens to be installed on the machine
+            """
+            Show-Elapsed {
+                $PSPrefix = Get-Module PSPrefix
+                $SyncHost = Get-SynchronizedHost
+
+                1 | ForEach-Object -Parallel {
+                    Import-Module $using:PSPrefix
+                    Show-Prefixed Inner { Write-Host Foo } -CustomHost $using:SyncHost
+                }
+
+            } -Module (Get-Module PSPrefix)
+            """
+        );
+
+        exception   .ShouldBeNull();
+        output.Count.ShouldBe(0);
     }
 }
